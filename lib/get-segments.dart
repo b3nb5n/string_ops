03ba0c6e42a090d949becffca515e36d64f4a988
Ben Baldwin <ben@baldwindesign.co>
2021-03-20 14:21:26 -0700

    improved segmentation regex

--- lib/get-segments.dart
+++ lib/get-segments.dart
@@ -1,5 +1,6 @@
 List<String> getSegments(String input) {
-  final segmentPattern = RegExp(r'[a-z][^A-Z\s_-]+|[A-Z][a-z0-9]+|[^\s_-]+');
+  final segmentPattern =
+      RegExp(r'[a-z0-9][a-z\d]+|[A-Z][a-z0-9]+|[A-Z][A-Z\d]+|[a-zA-Z\d]+');
 
   final matches = segmentPattern.allMatches(input).toList();
   return matches.map((match) => match.group(0)).toList();
