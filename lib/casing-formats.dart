1da45c1b1e7c3eb311780940ffc1d22c0851e633
Ben Baldwin <ben@baldwindesign.co>
2021-03-19 17:31:50 -0700

    casing formats

---
+++ lib/casing-formats.dart
@@ -0,0 +1,38 @@
+/// Accepted casing formats
+enum CasingFormat {
+  /// this is lower case
+  lowerCase,
+
+  /// THIS IS UPPER CASE
+  upperCase,
+
+  /// This is scentence case
+  scentenceCase,
+
+  /// This Is Title Case
+  titleCase,
+
+  /// thisIsCamelCase
+  camelCase,
+
+  /// ThisIsPascalCase
+  pascalCase,
+
+  /// this_is_snake_case
+  snakeCase,
+
+  /// THIS_IS_UPPER_SNAKE_CASE
+  upperSnakeCase,
+
+  /// This_Is_Title_Snake_Case
+  titleSnakeCase,
+
+  /// this-is-kebab-case
+  kebabCase,
+
+  /// THIS-IS-UPPER-KEBAB-CASE
+  upperKebabCase,
+
+  /// This-Is-Title-Kebab-Kase
+  titleKebabCase,
+}
